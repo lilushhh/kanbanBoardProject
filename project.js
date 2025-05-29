ef30d3edb6881ca983a3fc1cba491180290a30f0
lilushhh <lilyelimelech@gmail.com>
2025-05-29 12:44:49 +0300

    added name option

--- project.js
+++ project.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const projectKey = "tasks_" + projectName;
     const tasks = JSON.parse(localStorage.getItem(projectKey)) || [];
 
-    tasks.forEach(task => renderTask(task.text, task.status));
+    tasks.forEach(task => renderTask(task.text, task.status, task.name));
 
     const dropZones = document.querySelectorAll(".tasks");
 
@@ -25,33 +25,50 @@ document.addEventListener("DOMContentLoaded", () => {
             const text = e.dataTransfer.getData("text/plain");
             const oldStatus = e.dataTransfer.getData("status");
             const newStatus = zone.parentElement.id;
+            const nameTask = e.dataTransfer.getData("name");
 
             const oldColumn = document.querySelector(`#${oldStatus} .tasks`);
             const taskToMove = [...oldColumn.children].find(el => {
                 const span = el.querySelector("span");
-                return span && span.textContent === text;
+                return span && span.textContent === `${text} - ${nameTask}`;
             });
 
             if (taskToMove) {
                 zone.appendChild(taskToMove);
             }
+
+            const projectKey = "tasks_" + localStorage.getItem("currentProject");
             let tasks = JSON.parse(localStorage.getItem(projectKey) || "[]");
 
             tasks = tasks.map(task =>
-                task.text === text ? {...task, status: newStatus } : task
+                `${task.text} - ${task.name}` === `${text} - ${nameTask}`
+                    ? { ...task, status: newStatus }
+                    : task
             );
 
             localStorage.setItem(projectKey, JSON.stringify(tasks));
             updateTaskCounts();
         });
     });
-
 });
 
+let undefinedUsers = 0;
+
+function addName() {
+    const input = document.getElementById("nameInput");
+    let name = input.value.trim();
+    if (!name) {
+        undefinedUsers++;
+        name = `user ${undefinedUsers}`;
+    }
+    input.value = "";
+    return name;
+}
 
 function addTask() {
     const input = document.getElementById("taskInput");
     const taskText = input.value.trim();
+    const nameText = addName();
     if (!taskText) return;
 
     const projectName = localStorage.getItem("currentProject");
@@ -60,28 +77,29 @@ function addTask() {
     let raw = localStorage.getItem(projectKey);
     let tasks = raw ? JSON.parse(raw) : [];
 
-    tasks.push({ text: taskText, status: "todo" });
+    tasks.push({ text: taskText, status: "todo", name: nameText });
     localStorage.setItem(projectKey, JSON.stringify(tasks));
 
-    renderTask(taskText, "todo");
+    renderTask(taskText, "todo", nameText);
 
     input.value = "";
 }
 
-function renderTask(text, status) {
+function renderTask(text, status, name) {
     const task = document.createElement("div");
     task.classList.add("task");
     task.setAttribute("draggable", "true");
 
     task.innerHTML = `
-        <span>${text}</span>
-        <i class = "fas fa-trash delete-icon" onclick="deleteTask(this)"></i>
+        <span>${text} - ${name}</span>
+        <i class="fas fa-trash delete-icon" onclick="deleteTask(this)"></i>
     `;
 
     task.addEventListener("dragstart", (e) => {
         e.dataTransfer.setData("text/plain", text);
         const currentColumn = task.closest(".column").id;
         e.dataTransfer.setData("status", currentColumn);
+        e.dataTransfer.setData("name", name);
         setTimeout(() => {
             task.style.display = "none";
         }, 0);
@@ -90,7 +108,6 @@ function renderTask(text, status) {
     task.addEventListener("dragend", () => {
         task.style.display = "flex";
     });
-
 
     const column = document.querySelector(`#${status} .tasks`);
     column.appendChild(task);
@@ -118,7 +135,7 @@ function deleteTask(icon) {
     const projectKey = "tasks_" + projectName;
 
     let tasks = JSON.parse(localStorage.getItem(projectKey) || []);
-    tasks = tasks.filter(task => task.text !== taskText);
+    tasks = tasks.filter(task => `${task.text} - ${task.name}` !== taskText);
 
     localStorage.setItem(projectKey, JSON.stringify(tasks));
 
